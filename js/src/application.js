ecf3fd7f9547c5af8a12a5339064698345516cf4
Fredward <faandrade@gmail.com>
2015-06-13 17:42:20 -0400

    things

--- js/src/application.js
+++ js/src/application.js
@@ -10,5 +10,51 @@ App.ResetScroll = Ember.Mixin.create({
   }
 });
 
-App.ApplicationAdapter = DS.FixtureAdapter.extend({
-})
+// App.ApplicationAdapter = DS.FixtureAdapter.extend({
+// })
+// App.store = DS.Store.create({
+//   App.ApplicationAdapter = DS.RESTAdapter.create({
+//     namespace: "data",
+//     url: "http://localhost:5000/api/book",
+//     ajax: function (url, type, hash) {
+//       hash.url = url;
+//       hash.type = type;
+//       hash.dataType = 'jsonp';
+//       hash.contentType = 'application/json; charset=utf-8';
+//       hash.context = this;
+
+//         if (hash.data && type !== 'GET') {
+//             hash.data = JSON.stringify(hash.data);
+//         }
+
+//       jQuery.ajax(hash);
+//     },
+//   })
+// });
+
+App.store = DS.Store.create({
+    // revision: 11,
+    adapter: DS.RESTAdapter.create({
+        namespace: "data",
+        url: "http://localhost:5000/api/book",
+        ajax: function (url, type, hash) {
+            hash.url = url;
+            hash.type = type;
+            hash.dataType = 'jsonp';
+            hash.contentType = 'application/json; charset=utf-8';
+            hash.context = this;
+
+            if (hash.data && type !== 'GET') {
+                hash.data = JSON.stringify(hash.data);
+            }
+
+            jQuery.ajax(hash);
+        },
+    })
+});
+
+// App.IndexRoute = Ember.Route.extend({
+//   model: function() {
+//     return $.getJSON('http://localhost:5000/api/book');
+//   }
+// });
